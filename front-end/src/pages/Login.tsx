d035fd71c65565a8718750791eac6e9fdc2819a4
PhamDoanMinhTu <minhtupham157@gmail.com>
2024-09-30 23:41:19 +0700

    back login

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -1,8 +1,9 @@
+// src/pages/Login.tsx
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { login } from '../store/slices/authSlice';
+import { login } from '../store/slices/authSlice'; // Import login action
 import { useNavigate } from 'react-router-dom';
-import { Button, Input, Form, message } from 'antd';
+import { Button, Input, Form, Checkbox, message } from 'antd';
 import '../styles/LoginCustom.css';
 
 const Login: React.FC = () => {
@@ -12,6 +13,7 @@ const Login: React.FC = () => {
 
   const onFinish = async (values: { username: string; password: string }) => {
     setLoading(true);
+
     try {
       const apiPort = process.env.REACT_APP_API_PORT;
       const response = await fetch(`http://localhost:${apiPort}/auth/login`, {
@@ -27,11 +29,13 @@ const Login: React.FC = () => {
 
       if (response.ok) {
         const data = await response.json();
-        const token = data.token;
-        const userInfo = data.userInfo;
+        const token = data.token; // Token from API response
+        const userInfo = data.userInfo; // Giả sử API trả về thông tin người dùng như `maNhanVien`, `name`, và `email`
+
+        // Dispatch login action với token và thông tin người dùng
         dispatch(login({ token, userInfo }));
         message.success('Đăng nhập thành công!');
-        navigate('/');
+        navigate('/'); // Redirect to home page after successful login
       } else {
         message.error('Sai tài khoản hoặc mật khẩu!');
       }
@@ -43,60 +47,43 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div>
-      <div style={{ 
-          position: 'absolute', 
-          display: 'flex', 
-          justifyContent: 'flex-start', 
-          alignItems: 'center', 
-          padding: '10px'
-        }}>
-        <img
-          style={{ width: '50px', height: '50px', marginTop: '5px', borderRadius: '15px' }}
-          src="https://res.cloudinary.com/dhyt592i7/image/upload/v1726735463/s7noroyd5bk6whepwsbu.png"
-          alt="Logo"
-        />
-          <h1 style={{ color: 'black', marginLeft: '15px' }}>Trung Tâm Prometheus</h1>
-      </div>
-
-      {/* Phần chính của trang login */}
-      <div className="login-container">
-        <div className="login-left">
-          <h1 className="login-title">Đăng Nhập</h1>
-
-          <Form onFinish={onFinish}>
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: 'Vui lòng nhập tài khoản!' }]}>
-              <Input placeholder="tài khoản" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              className="input-pw"
-              rules={[{ required: true, message: 'Vui lòng nhập password!' }]}>
-              <Input.Password placeholder="password" />
-            </Form.Item>
-            <Form.Item>
-              <a href="/forgot-password" className="forgot-password">
-                Forgotpassword ？
-              </a>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-submit-btn" loading={loading}>
-                Đăng Nhập
-              </Button>
-            </Form.Item>
-          </Form>
+    <div className="login-page">
+            <div className="login-form">
+                <div className='login-txt'><h1>Đăng Nhập</h1></div>
+                <Form onFinish={onFinish}>
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản!' }]}
+                    >
+                        <Input placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}
+                    >
+                        <Input.Password placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                    <Form.Item>
+                        <a href="/forgot-password" style={{ float: 'right' }}>Forgot Password?</a>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" loading={loading}>
+                            Log In
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+            <div className="login-image">
+                <img
+                    src="https://res.cloudinary.com/dhyt592i7/image/upload/v1727456258/yfdoz0fxkdpcnitiuosm.png"
+                    alt="Login Illustration"
+                    style={{ width: '100%' }}
+                />
+            </div>
         </div>
-        <div className="login-right">
-          <img
-            src="https://res.cloudinary.com/dhyt592i7/image/upload/v1727710516/lyqhfgmnasdhwkhheqpx.png"
-            alt="Login Illustration"
-            className="login-illustration"
-          />
-        </div>
-      </div>
-    </div>
   );
 };
 
