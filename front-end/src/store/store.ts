f280d4cc1866e01defb4381aa721d4e3f0201196
PhamDoanMinhTu <144188315+PhamDoanMinhTu@users.noreply.github.com>
2024-10-19 23:48:59 +0700

    update reload tai khoan, chi hien nhung ma nv chua co tk

--- front-end/src/store/store.ts
+++ front-end/src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice'; // Import auth reducer
 import nhanVienReducer from './slices/nhanVienSlice'; // Import nhanVien reducer
 import hocVienReducer from './slices/hocVienSlice'; // Import hocVien reducer
+import taiKhoanReducer from './slices/taiKhoanSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // Thêm auth reducer
     nhanvien: nhanVienReducer, // Thêm nhanVien reducer
     hocvien: hocVienReducer, // Thêm hocVien reducer
+    taikhoan: taiKhoanReducer, 
   },
 });
 
