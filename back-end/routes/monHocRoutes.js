313169f5f76503e9fa0912a6a3c7581d6fa72c4a
Smile005 <haducanh1802@gmail.com>
2024-10-19 16:33:28 +0700

    update

--- back-end/routes/monHocRoutes.js
+++ back-end/routes/monHocRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getMonHoc, createMonHoc, updateMonHoc } = require('../controllers/monHocController');
+const { getMonHoc, createMonHoc, updateMonHoc, xoaMonHoc } = require('../controllers/monHocController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/ds-monhoc', authMiddleware(2), getMonHoc); 
 router.post('/them-monhoc', authMiddleware(2), createMonHoc); 
-router.put('/sua-monhoc', authMiddleware(2), updateMonHoc); 
+router.put('/sua-monhoc', authMiddleware(2), updateMonHoc);
+router.post('/xoa-monHoc', authMiddleware(2), xoaMonHoc); 
 
 module.exports = router;
