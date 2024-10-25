e690dacfcce2f6e8344d1a825111e044e4bc8cfa
Smile005 <haducanh1802@gmail.com>
2024-10-25 13:33:03 +0700

    update

--- back-end/controllers/dsLopHocController.js
+++ back-end/controllers/dsLopHocController.js
@@ -1,30 +1,12 @@
 const pool = require('../config/db');
 
 const getDS_Lop = async (req, res) => {
-    const { maLopHoc } = req.body;
-
     try {
-        const [results] = await pool.query(`
-            SELECT 
-                hv.maHocVien,
-                hv.tenHocVien,
-                dl.ghiChu
-            FROM 
-                DsLopHoc dl
-            JOIN 
-                HocVien hv ON dl.maHocVien = hv.maHocVien
-            WHERE 
-                dl.maLopHoc = ?
-        `, [maLopHoc]);
-
-        if (results.length === 0) {
-            return res.status(404).json({ message: 'Không tìm thấy học viên cho mã lớp học này' });
-        }
-
+        const [results] = await pool.query('SELECT * FROM DsLopHoc');
         res.json(results);
-    } catch (error) {
-        res.status(500).json({ message: 'Không thể lấy danh sách học viên', error });
-    }
+      } catch (error) {
+        res.status(500).json({ message: 'Lỗi server', error });
+      }
 };
 
 const getDS_Lop02 = async (req, res) => {
